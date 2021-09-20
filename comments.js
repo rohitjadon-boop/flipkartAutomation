9e7feeb64d5fa201fdbcc446e71e08143bb7f5b7
delete <84199840+rohitjadon-boop@users.noreply.github.com>
2021-09-20 18:56:05 +0530

    Add files via upload

---
+++ comments.js
@@ -0,0 +1,11 @@
+const comments = {
+    productName: '👏 Please Enter Valid ProductName......ThankYou',
+    productPrice: '👏 Please Enter Valid Price......Thank You',
+    productNameAndPrice: '👏 Please Enter The Valid Price In Order To Proceed Further.....Thank You',
+    productRating: '👏 Please Enter Valid Rating......Thank You'
+
+}
+
+module.exports = {
+    comments: comments,
+};
